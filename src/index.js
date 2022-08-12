769be60a67dd02eb6a1f4ab3f312348806e04119
Yauheni <jengulya4211@gmail.com>
2022-08-12 21:03:55 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let isDeleteElement = true;
+  while (str.length > 0 && isDeleteElement) {
+    isDeleteElement = false;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let brackets = bracketsConfig[i].join('');
+      while (str.length > 0 && str.includes(brackets)) {
+        isDeleteElement = true;
+        str = str.replaceAll(brackets, '');
+      }
+    }
+  }
+  if (str.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
+
+
+
+
+
 }
